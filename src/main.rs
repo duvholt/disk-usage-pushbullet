26d2d5cba8b27caa91a6e13e4f81eb60fbbb2f75
Christian Duvholt <christian@duvholt.net>
2020-06-30 22:06:54 +0200

    Push if disk usage keeps increasing

--- src/main.rs
+++ src/main.rs
@@ -45,7 +45,7 @@ fn push(log: &slog::Logger, percentage: f64) -> Result<(), String> {
     }
 }
 
-fn check_disk_usage(log: &slog::Logger, treshold: f64, pushed: bool) -> Result<bool, String> {
+fn disk_usage(log: &slog::Logger) -> Result<f64, String> {
     debug!(log, "Checking disk usage");
     let sys = System::new();
     let root_mount = sys
@@ -57,17 +57,32 @@ fn check_disk_usage(log: &slog::Logger, treshold: f64, pushed: bool) -> Result<b
                 .find(|mount| mount.fs_mounted_on == "/")
                 .ok_or_else(|| "Unable to find root mount".to_owned())
         })?;
-    let percentage = root_mount.avail.as_u64() as f64 / root_mount.total.as_u64() as f64;
-    let log = log.new(o!("percentage" => format!("{:.2}", percentage)));
+    Ok(root_mount.avail.as_u64() as f64 / root_mount.total.as_u64() as f64)
+}
+
+fn should_push(percentage: f64, treshold: f64, previous_percentage: f64) -> bool {
+    if percentage >= treshold {
+        return false;
+    }
+    // If treshold is met we want to avoid spamming unless disk usage keeps increasing
+    (percentage * 100.0).floor() < (previous_percentage * 100.0).floor()
+}
+
+fn check_disk_usage(
+    log: &slog::Logger,
+    percentage: f64,
+    treshold: f64,
+    previous_percentage: f64,
+) -> Result<f64, String> {
     if percentage < treshold {
         info!(log, "Low disk space treshold met");
-        if !pushed {
-            push(&log, percentage)?;
-        }
     } else {
         debug!(log, "Low disk space treshold not met");
     }
-    Ok(percentage < treshold)
+    if should_push(percentage, treshold, previous_percentage) {
+        push(&log, percentage)?;
+    }
+    Ok(percentage)
 }
 
 fn main() {
@@ -78,19 +93,127 @@ fn main() {
     let treshold = 0.1;
     let sleep_time = Duration::from_secs(60 * 5);
 
-    let mut pushed = false;
+    let mut previous_percentage = 1.0;
 
     info!(root, "Starting disk-warn");
-    let disk_log = root.new(o!("treshold" => treshold, "sleep_time" => sleep_time.as_secs()));
+    let log = root.new(o!("treshold" => treshold, "sleep_time" => sleep_time.as_secs()));
     loop {
-        match check_disk_usage(&disk_log, treshold, pushed) {
-            Ok(warned) => {
-                pushed = warned;
+        let log = log.new(o!("previous" => format!("{:.2}", previous_percentage)));
+        let percentage = match disk_usage(&log) {
+            Ok(percentage) => percentage,
+            Err(err) => {
+                error!(log, "Got error while calculating disk usage: {:#?}", err);
+                continue;
+            }
+        };
+        let log = log.new(o!("percentage" => format!("{:.2}", percentage)));
+        match check_disk_usage(&log, percentage, treshold, previous_percentage) {
+            Ok(percentage) => {
+                previous_percentage = percentage;
             }
             Err(err) => {
-                error!(disk_log, "Got error while checking disk usage: {:#?}", err);
+                error!(log, "Got error while checking disk usage: {:#?}", err);
             }
         }
         thread::sleep(sleep_time);
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn test_should_push_initial_below_treshold() {
+        let percentage = 0.09;
+        let previous = 1.0;
+        let treshold = 0.1;
+
+        let push = should_push(percentage, treshold, previous);
+
+        assert!(
+            push,
+            "Push should be true when below treshold and previous above treshold"
+        );
+    }
+
+    #[test]
+    fn test_should_not_push_initial_above_treshold() {
+        let percentage = 0.20;
+        let previous = 0.3;
+        let treshold = 0.1;
+
+        let push = should_push(percentage, treshold, previous);
+
+        assert!(
+            !push,
+            "Push should be true when below treshold and previous above treshold"
+        );
+    }
+
+    #[test]
+    fn test_should_not_push_unchanged() {
+        let percentage = 0.09;
+        let previous = 0.09;
+        let treshold = 0.1;
+
+        let push = should_push(percentage, treshold, previous);
+
+        assert!(!push, "Push should be false when there is no change");
+    }
+
+    #[test]
+    fn test_should_not_push_less_than_one_percent() {
+        let percentage = 0.09;
+        let previous = 0.09;
+        let treshold = 0.1;
+
+        let push = should_push(percentage, treshold, previous);
+
+        assert!(
+            !push,
+            "Push should be false when less than one percent has changed"
+        );
+    }
+
+    #[test]
+    fn test_should_push_more_than_one_percent() {
+        let percentage = 0.08;
+        let previous = 0.09;
+        let treshold = 0.1;
+
+        let push = should_push(percentage, treshold, previous);
+
+        assert!(
+            push,
+            "Push should be false when less than one percent has changed"
+        );
+    }
+
+    #[test]
+    fn test_should_not_push_increasing() {
+        let percentage = 0.09;
+        let previous = 0.08;
+        let treshold = 0.1;
+
+        let push = should_push(percentage, treshold, previous);
+
+        assert!(
+            !push,
+            "Push should be false when less than one percent has changed"
+        );
+    }
+
+    #[test]
+    fn test_should_not_push_increase_above_treshold() {
+        let percentage = 0.11;
+        let previous = 0.09;
+        let treshold = 0.1;
+
+        let push = should_push(percentage, treshold, previous);
+
+        assert!(
+            !push,
+            "Push should be false when less than one percent has changed"
+        );
     }
 }
